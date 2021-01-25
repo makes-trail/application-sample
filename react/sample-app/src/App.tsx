a355acb4558cfa7d71f10f60e6704c5845a20ea4
batakoo3 <chuanduanku52@gmail.com>
2021-01-25 22:35:20 +0900

    Refactor: organize folder structure

--- react/sample-app/src/App.tsx
+++ react/sample-app/src/App.tsx
@@ -1,23 +1,17 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Book from './Book';
+import BookInfo from './components/BookInfo';
+import { Book } from './types/Book';
 
 function App() {
-  interface BookInfo {
-    isbn: string;
-    title: string;
-    author: string;
-    publisher: string;
-    cover: string;
-  }
 
   const regex = new RegExp(/^\d{13}$/);
   const [isbn, setIsbn] = useState<string>("9784");
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [isPending, setIsPending] = useState<boolean>(false);
   const [isNoData, setIsNoData] = useState<boolean>(false);
-  const [bookInfo, setBookInfo] = useState<BookInfo>();
+  const [book, setBook] = useState<Book>();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,7 +24,7 @@ function App() {
     setErrorMessage("");
     setIsPending(true);
     setIsNoData(false);
-    setBookInfo(undefined);
+    setBook(undefined);
     const url = new URL(`${process.env.REACT_APP_SERVERLESS_ENDPOINT}/fetch-isbn-summary`);
     const params = new URLSearchParams();
     params.append("isbn", isbn);
@@ -44,13 +38,13 @@ function App() {
           setIsNoData(true);
           return;
         }
-        const data: Array<BookInfo> = await res.json();
+        const data: Array<Book> = await res.json();
         if (data.length === 0) {
           setIsPending(false);
           setIsNoData(true);
           return;
         }
-        setBookInfo(data[0]);
+        setBook(data[0]);
         setIsPending(false);
       }, 2000
     )
@@ -83,7 +77,7 @@ function App() {
         <div>
           {isPending && <p>データ取得中。ちょっと待ってね...</p>}
           {!isPending && isNoData && <p>書籍情報がなかったよ。ぴえん</p>}
-          {!isPending && bookInfo && <Book bookInfo={bookInfo} />}
+          {!isPending && book && <BookInfo book={book} />}
         </div>
       </div>
     </div>
