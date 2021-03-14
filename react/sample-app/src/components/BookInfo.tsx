f6a3fb0f9c13e53275236562e335a4ee5d27fdb1
batakoo3 <chuanduanku52@gmail.com>
2021-03-14 23:57:12 +0900

    Add feature to save book information

--- react/sample-app/src/components/BookInfo.tsx
+++ react/sample-app/src/components/BookInfo.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import http from '../common/http';
 import { Book } from '../types/Book';
 
 const BookInfo: React.FC<{ book: Book }> = ({ book }) => {
+  const saveBookInfo = async (isbn: string) => {
+    const urlOpenbd = new URL(`${process.env.REACT_APP_API_ENDPOINT}/save-openbd/${isbn}`);
+    const urlGbooks = new URL(`${process.env.REACT_APP_API_ENDPOINT}/save-gbooks/${isbn}`);
+    const resOpenbd = await http.put(urlOpenbd.toString());
+    const resGbooks = await http.put(urlGbooks.toString());
+    if (resOpenbd.status === 204 && resGbooks.status === 204) {
+      alert("書籍情報がDynamoDBに保存されたよ！")
+    }
+  }
+
   if (!book) {
     return null;
   }
   return (
-    <div>
-      <p>{book.isbn}</p>
-      <p>{book.title}</p>
-      <p>{book.author}</p>
-      <p>{book.publisher}</p>
-      {book.cover && <img
-        src={book.cover}
-        alt=""
-      />}
+    <div className="font-yumin">
+      <div>
+        <p>{book.isbn}</p>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <p>{book.publisher}</p>
+        {book.cover && <img
+          src={book.cover}
+          alt=""
+        />}
+      </div>
+      <button className="font-yumin" onClick={() => saveBookInfo(book.isbn)}>保存</button>
     </div>
   );
 }
