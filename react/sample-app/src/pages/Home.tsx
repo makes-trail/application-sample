70d6e00abd9e7a4178524ea3a6c9f2510bc114b4
batakoo3 <chuanduanku52@gmail.com>
2021-03-18 21:00:49 +0900

    Fix typo

--- react/sample-app/src/pages/Home.tsx
+++ react/sample-app/src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
 
   return (
     <div>
-      <h1 className="font-yumin">MAKE TRAIL</h1>
+      <h1 className="font-yumin">MAKES TRAIL</h1>
       <h2 className="font-yumin">サンプル版</h2>
       <Link to={"/search-book"}>
         <p className="font-yumin">クリックして始める</p>
