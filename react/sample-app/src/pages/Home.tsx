5cc13b41485ad7f2f283f184456e9207f36404ec
batakoo3 <chuanduanku52@gmail.com>
2021-04-18 12:00:49 +0900

    Add feature: list books stoired in RDS

--- react/sample-app/src/pages/Home.tsx
+++ react/sample-app/src/pages/Home.tsx
@@ -8,7 +8,10 @@ const Home: React.FC = () => {
       <h1 className="font-yumin">MAKES TRAIL</h1>
       <h2 className="font-yumin">サンプル版</h2>
       <Link to={"/search-book"}>
-        <p className="font-yumin">クリックして始める</p>
+        <p className="font-yumin">本を探す</p>
+      </Link>
+      <Link to={"/list-books"}>
+        <p className="font-yumin">図書館に入る</p>
       </Link>
     </div>
   )
---
+++ react/sample-app/src/pages/ListBooks.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import http from '../common/http';
+import { Book } from '../types/Book';
+import BookCard from '../components/BookCard';
+
+const ListBooks: React.FC = () => {
+  const [books, setBooks] = useState<Array<Book>>();
+
+  useEffect(() => {
+    const url = new URL(`${process.env.REACT_APP_API_ENDPOINT}/list`);
+
+    const source = axios.CancelToken.source();
+    const fetchBookList = async () => {
+      const res = await http.get(url.toString(), {
+        cancelToken: source.token
+      });
+      const data: Array<Book> = await res.data;
+      setBooks(data);
+    }
+    fetchBookList();
+
+    return () => source.cancel("Component got unmounted");
+  }, []);
+
+  return (
+    <div>
+      <h2 className="title font-yumin">
+        みんなが保存した本たち
+      </h2>
+      <Link to={"/"}>
+        <p className="font-yumin">Homeに戻る</p>
+      </Link>
+      <Container>
+        <Grid container justify="center" spacing={3}>
+          {books && books.map(book => (
+            <Grid item key={book.isbn} xs={12} sm={6}>
+              <BookCard book={book} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  )
+}
+
+export default ListBooks;
