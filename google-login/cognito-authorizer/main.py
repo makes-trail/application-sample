82d4a89cacab2287454a412a06a241a05cc111d5
batakoo3 <chuanduanku52@gmail.com>
2021-12-03 22:25:10 +0900

    Cognitoオーソライザ付きAPI Gatewayを追加

---
+++ google-login/cognito-authorizer/main.py
@@ -0,0 +1,20 @@
+import json
+
+
+def handler(event, context):
+    print(event)
+
+    try:
+        claims = event["requestContext"]["authorizer"]["claims"]
+        user_info = {
+            "userId": claims["cognito:username"],
+            "name": claims["name"],
+            "iconUrl": claims["picture"],
+        }
+        print(f'User logged in: {claims["name"]}')
+
+        return {"statusCode": 200, "body": json.dumps(user_info)}
+
+    except Exception as e:
+        print(e)
+        return {"statusCode": 500, "body": json.dumps("ERROR")}
